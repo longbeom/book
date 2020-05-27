61fabbabb8f999735ab3abdce1ac6ba675503ac2
longbeom <wowow1859@naver.com>
2020-05-27 23:51:59 +0900

    ADD : Description (HelloResponseDtoTest)

--- src/test/java/com/longbeom/book/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/longbeom/book/web/dto/HelloResponseDtoTest.java
@@ -18,5 +18,18 @@ public class HelloResponseDtoTest {
         //then
         assertThat(dto.getName()).isEqualTo(name);
         assertThat(dto.getAmount()).isEqualTo(amount);
+
+        /**
+         * assertThat
+         * - assertj 라는 테스트 검증 라이브러리의 검증 메소드
+         * - 검증하고 싶은 대상을 메소드 인자로 받는다.
+         * - 메소드 체이닝이 지원되어 isEqualTo 와 같이 메소드를 이어서 사용할 수 있다.
+         */
+
+        /**
+         * isEqualTo
+         * - assertj 의 동등 비교 메소드
+         * - assertThat 에 있는 값과 isEqualTo 의 값을 비교해서 같을 때만 성공
+         */
     }
 }
