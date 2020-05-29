a5e65489acd150a1113dfa4a3417b4adada394ca
longbeom <wowow1859@naver.com>
2020-05-30 01:46:30 +0900

    ADD : create Posts & Test

---
+++ src/main/java/com/longbeom/book/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.longbeom.book.service.posts;
+
+import com.longbeom.book.domain.posts.PostsRepository;
+import com.longbeom.book.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
