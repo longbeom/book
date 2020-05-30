b4bca33e021a6cb2969336b3d2425345d0f68c8c
longbeom <wowow1859@naver.com>
2020-05-31 02:02:20 +0900

    ADD : update Posts & Test

--- src/main/java/com/longbeom/book/service/posts/PostsService.java
+++ src/main/java/com/longbeom/book/service/posts/PostsService.java
@@ -1,7 +1,9 @@
 package com.longbeom.book.service.posts;
 
+import com.longbeom.book.domain.posts.Posts;
 import com.longbeom.book.domain.posts.PostsRepository;
 import com.longbeom.book.web.dto.PostsSaveRequestDto;
+import com.longbeom.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +17,13 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
     }
 }
--- src/main/java/com/longbeom/book/web/PostsApiController.java
+++ src/main/java/com/longbeom/book/web/PostsApiController.java
@@ -2,10 +2,9 @@ package com.longbeom.book.web;
 
 import com.longbeom.book.service.posts.PostsService;
 import com.longbeom.book.web.dto.PostsSaveRequestDto;
+import com.longbeom.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +15,11 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,
+                       @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 }
