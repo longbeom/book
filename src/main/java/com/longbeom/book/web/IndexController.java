9d4ea76c2a93bb3d31180b3a745c431b93f51443
longbeom <wowow1859@naver.com>
2020-05-31 15:08:36 +0900

    ADD : plugin mustache, Index Page & Test

---
+++ src/main/java/com/longbeom/book/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.longbeom.book.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
