982157657d152acdadddcdc2c4797b251f02d007
longbeom <wowow1859@naver.com>
2020-05-31 16:19:06 +0900

    ADD : add get view (posts)

--- src/main/java/com/longbeom/book/service/posts/PostsService.java
+++ src/main/java/com/longbeom/book/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.longbeom.book.service.posts;
 
 import com.longbeom.book.domain.posts.Posts;
 import com.longbeom.book.domain.posts.PostsRepository;
+import com.longbeom.book.web.dto.PostsListResponseDto;
 import com.longbeom.book.web.dto.PostsResponseDto;
 import com.longbeom.book.web.dto.PostsSaveRequestDto;
 import com.longbeom.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/longbeom/book/web/IndexController.java
+++ src/main/java/com/longbeom/book/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.longbeom.book.web;
 
+import com.longbeom.book.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
