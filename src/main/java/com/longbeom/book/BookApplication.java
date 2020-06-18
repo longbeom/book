c073f420b15a3196a738ea387a0792ba2e2e97fc
longbeom <wowow1859@naver.com>
2020-06-18 23:55:35 +0900

    FIX : @EnableJpaAuditing 분리

--- src/main/java/com/longbeom/book/BookApplication.java
+++ src/main/java/com/longbeom/book/BookApplication.java
@@ -2,9 +2,7 @@ package com.longbeom.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing
 @SpringBootApplication
 public class BookApplication {
     public static void main(String[] args) {
